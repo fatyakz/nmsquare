ffb728232112e4929ad384891fd24e9c225f3e13
fatyakz <106924302+fatyakz@users.noreply.github.com>
2022-09-03 10:42:24 +1000

    Update nmsquare.cpp

--- nmsquare/nmsquare.cpp
+++ nmsquare/nmsquare.cpp
@@ -13,6 +13,7 @@ struct S_matches {
     int n;
     int m;
     int e;
+    int root;
 };
 
 static S_matches g_best;
@@ -55,17 +56,17 @@ static S_matches thr_SquareGen(unsigned long long int t_start, unsigned long lon
                 std::cout << "GOTTEM E:" << E << " n:" << lN << " m:" << lM << "\n";
             flash:
                 t_cycles++;
-                if (t_matches > t_best.matches) { t_best.e = E; t_best.n = lN; t_best.m = lM; t_best.matches = t_matches + 1; }
+                if (t_matches > t_best.matches) { t_best.e = E; t_best.n = lN; t_best.m = lM; t_best.matches = t_matches + 1; t_best.root = i; }
                 t_matches = 0;
             }
         }
     }
     mlock.lock();
     if (t_best.matches > g_best.matches) {
-        g_best.e = E; g_best.n = t_best.n; g_best.m = t_best.m; g_best.matches = t_best.matches;
+        g_best.e = E; g_best.n = t_best.n; g_best.m = t_best.m; g_best.matches = t_best.matches; g_best.root = t_best.root;
     }
     std::cout << "thread " << t_start << ": " << t_cycles << " cycles, E="<< E << 
-        " t_best=" << t_best.matches << " e=" << t_best.e << " n=" << t_best.n << " m=" << t_best.m << "\n";
+        " t_best=" << t_best.matches << " e=" << t_best.e << " n=" << t_best.n << " m=" << t_best.m << " r=" << t_best.root << "\n";
     mlock.unlock();
     return t_best;
 }
@@ -94,12 +95,12 @@ top:
         high_resolution_clock::time_point t2 = high_resolution_clock::now();
         duration<double> total_time = duration_cast<duration<double>>(t2 - t1);
         std::cout << "Block time : " << total_time.count() << " s\n\n";
-        std::cout << "CURRENT g_best=" << g_best.matches << " e=" << g_best.e << " n=" << g_best.n << " m=" << g_best.m << "\n\n";
+        std::cout << "CURRENT g_best=" << g_best.matches << " e=" << g_best.e << " n=" << g_best.n << " m=" << g_best.m << " r=" << g_best.root <<"\n\n";
     }
     high_resolution_clock::time_point g2 = high_resolution_clock::now();
     duration<double> g_total_time = duration_cast<duration<double>>(g2 - g1);
     std::cout << "Total time : " << g_total_time.count() << " s\n";
-    std::cout << "FINAL g_best=" << g_best.matches << " e=" << g_best.e << " n=" << g_best.n << " m=" << g_best.m << "\n\n";
+    std::cout << "FINAL g_best=" << g_best.matches << " e=" << g_best.e << " n=" << g_best.n << " m=" << g_best.m << " r=" << g_best.root << "\n\n";
 
     goto top;
     return 0;
