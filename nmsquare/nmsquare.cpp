3856d3ad7977fe675f5204ab160ef4bc8f5949e4
JoJo <JoJo@IRIS>
2022-10-11 20:07:22 +1100

    -

--- nmsquare/nmsquare.cpp
+++ nmsquare/nmsquare.cpp
@@ -233,6 +233,48 @@ format format_long(unsigned long long num) {
     return f;
 }
 
+namespace color {
+    enum Code {
+        FG_RED = 31,
+        FG_GREEN = 32,
+        FG_BLUE = 34,
+        FG_DEFAULT = 39,
+        BG_RED = 41,
+        BG_GREEN = 42,
+        BG_BLUE = 44,
+        FG_DEFAULT = 39, 
+        FG_BLACK = 30, 
+        FG_RED = 31, 
+        FG_GREEN = 32, 
+        FG_YELLOW = 33, 
+        FG_BLUE = 34, 
+        FG_MAGENTA = 35, 
+        FG_CYAN = 36, 
+        FG_LIGHT_GRAY = 37, 
+        FG_DARK_GRAY = 90, 
+        FG_LIGHT_RED = 91, 
+        FG_LIGHT_GREEN = 92, 
+        FG_LIGHT_YELLOW = 93, 
+        FG_LIGHT_BLUE = 94, 
+        FG_LIGHT_MAGENTA = 95, 
+        FG_LIGHT_CYAN = 96, 
+        FG_WHITE = 97, 
+        BG_RED = 41, 
+        BG_GREEN = 42, 
+        BG_BLUE = 44, 
+        BG_DEFAULT = 49
+    };
+    class set {
+        Code code;
+    public:
+        set(Code pCode) : code(pCode) {}
+        friend std::ostream&
+            operator<<(std::ostream& os, const set& mod) {
+            return os << "\033[" << mod.code << "m";
+        }
+    };
+}
+
 class C_rmw {
 public:
     uint_fast64_t                           rmw_reads;
