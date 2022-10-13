8501805630c182ae3c5fbc876b0ffcd107e44bf6
JoJo <JoJo@IRIS>
2022-10-13 23:40:38 +1100

    -

--- nmsquare/nmsquare.cpp
+++ nmsquare/nmsquare.cpp
@@ -1566,7 +1566,7 @@ loophead:
 
 			rmw.ReadMergeWrite(global.G_BLOCK_FILE_PATH);
 
-			uint_fast64_t   predicted_cycles        = (((g_block.id + g_block.id) - 1) * ((g_block.id + g_block.id) - 1)) * global.G_NUM_THREADS;
+			uint_fast64_t   predicted_cycles        = (((g_block.id + g_block.id) - 1) * ((g_block.id + g_block.id) - 1));
 			double          predicted_cps           = rmw.GetAverageCPS(global.G_SYSTEM_NAME, global.var.G_AVG_CPS_RANGE);
 			double          predicted_seconds       = (predicted_cycles / predicted_cps) / global.G_NUM_THREADS;
 			uint_fast64_t   predicted_total_cycles  = 0;
