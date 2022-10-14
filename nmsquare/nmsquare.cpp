25c9be15bc9fc8d0dee73e241b20074a29f145a7
JoJo <JoJo@IRIS>
2022-10-14 23:13:06 +1100

    -

--- nmsquare/nmsquare.cpp
+++ nmsquare/nmsquare.cpp
@@ -1573,7 +1573,7 @@ loophead:
 			double          predicted_totalseconds = 0.0f;
 
 			for (uint_fast64_t i = g_block.id; i < global.G_BLOCK_START + global.G_LIMIT; i++) {
-				predicted_total_cycles += (((i + i) - 1) * ((i + i) - 1)) * global.G_NUM_THREADS;
+				predicted_total_cycles += (((i + i) - 1) * ((i + i) - 1));
 				predicted_totalseconds += (predicted_total_cycles / rmw.GetAverageCPS(global.G_SYSTEM_NAME, global.var.G_AVG_CPS_RANGE))
 					/ global.G_NUM_THREADS;
 			}
